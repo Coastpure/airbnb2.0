4ecf2c78b900c3aa8b72abf6d7e5cf05483a7f80
coastpure <njorogesimon465@gmail.com>
2023-12-07 18:55:28 +0300

    grad

--- src/components/Rental.jsx
+++ src/components/Rental.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Rental = ({title, image, price}) => {
   return (
     <div className='relative'>
+      <div className="grad absolute w-full h-full"></div>
         <div className="flex">
+          <div className=""></div>
             <img src = {image} alt="" 
                 className='object-cover rounded-[1.3rem]'/>
             <div className="absolute text-white font-bold bottom-0">{title}</div>
